5b93202c16fb8672b4db05c065702d0dbf057712
CodedMarc <doramm4408@gmail.com>
2022-09-28 17:43:19 -0500

    Mobile Refactor

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
 import './App.css';
 import axios from 'axios'
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Coin from './Coin.js'
 
 function App() {
 
   const [coins, setCoins] = useState([]);
   const [search, setSearch] = useState('');
-
-
-  useEffect(()=>{
-    axios.get(url)
-    .then(res=>{
-      setCoins(res.data);
-    })
-    .catch(e => console.log(e))
-}, [])
   const url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false";
-  
+
+
+  useEffect(() => {
+    axios.get(url)
+      .then(res => {
+        console.log(res.data)
+        setCoins(res.data);
+      })
+      .catch(e => console.log(e))
+  }, [])
+
   const handleChange = e => {
     setSearch(e.target.value)
   }
-  
-  const filteredCoins = coins.filter(coin =>
-    coin.name.toLowerCase().includes(search.toLowerCase())
-    )
-  
-  
+
+  const filteredCoins = coins.filter(coin => {
+    return coin.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+
+
   return (
     <div className="coin-app">
       <div className="coin-search">
         <h1 className="coin-text">Cryptracker</h1>
         <form>
-          <input 
-          type="text" 
-          placeholder="Search Coins..."
-          className='coin-input'
-          onChange={handleChange}
-          />   
+          <input
+            type="text"
+            placeholder="Search Coins..."
+            className='coin-input'
+            onChange={handleChange}
+          />
 
         </form>
+
         <div className="coin-labels">
           <h3 className="name-label">coin</h3>
           <h3 className="price-label">price</h3>
@@ -51,20 +54,20 @@ function App() {
 
       {filteredCoins.map(coin => {
         return (
-          <Coin 
-          key={coin.id}
-          name={coin.name}
-          image={coin.image}
-          symbol={coin.symbol}
-          volume={coin.total_volume}
-          price={coin.current_price}
-          priceChange={coin.price_change_percentage_24h}
-          marketcap={coin.market_cap}
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            image={coin.image}
+            symbol={coin.symbol}
+            volume={coin.total_volume}
+            price={coin.current_price}
+            priceChange={coin.price_change_percentage_24h}
+            marketcap={coin.market_cap}
           />
         )
       })}
 
-     
+
     </div>
   );
 }
--- src/Coin.js
+++ src/Coin.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import './Coin.css'
 
-const Coin = ({name, image, symbol, price, volume, priceChange, marketcap}) => {
+const Coin = ({ name, image, symbol, price, volume, priceChange, marketcap }) => {
     return (
-    
+
         <div className='coin-container'>
-            
+
             <div className="coin-row">
-                
+
                 <div className="coin">
                     <img src={image} alt='crypto' />
                     <h1>{name}</h1>
                     <p className='coin-symbol'>{symbol}</p>
                 </div>
-                <div className="coin-data">
-                    <p className="coin-price">${price.toLocaleString()}</p>
-                    <p className='coin-volume'>${volume.toLocaleString()}</p>
-                    {priceChange < 0 ? (
-                        <p className='coin-percent red'>{priceChange.toFixed(3)}%</p>
-                    ) : (
-                        <p className='coin-percent green'>{priceChange.toFixed(3)}%</p>
-                    )
-                }
-                    <p className="coin-marketcap">
-                        ${marketcap.toLocaleString()}
-                    </p>
+                <div className="mobile-container">
+                    <div className="mobile-labels">
+                        <h3>Price</h3>
+                        <h3>Volume</h3>
+                        <h3>+/- 24hr</h3>
+                        <h3>Market Cap</h3>
+                    </div>
+                    <div className="mobile-data">
+                        <div className="coin-data">
+                            <p className="coin-price">${price.toLocaleString()}</p>
+                            <p className='coin-volume'>${volume.toLocaleString()}</p>
+                            {priceChange < 0 ? (
+                                <p className='coin-percent red'>{priceChange.toFixed(2)}%</p>
+                            ) : (
+                                <p className='coin-percent green'>{priceChange.toFixed(2)}%</p>
+                            )
+                            }
+                            <p className="coin-marketcap">
+                                ${marketcap.toLocaleString()}
+                            </p>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
